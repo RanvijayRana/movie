17e6e00d5ddb2690eaf1b0f6619e62bb6f7ad13e
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-25 23:38:50 +0530

    initial analysis

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(() => {
 	let myData = "http://www.omdbapi.com/?i=tt3896198&apikey=bfabb086";
-  alert(myData);
+  //alert(myData);
   console.log(myData);
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    async: true,
+    url: 'http://www.omdbapi.com/?i=tt3896198&apikey=bfabb086',
+    success: (response) => {
+      $(".result").append(response.Title);
+    //  alert(response);
+    }
+  });
 });
