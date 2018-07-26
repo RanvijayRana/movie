afd41faa879aebc79a0097b4f493965ae3b9d9e5
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-26 06:08:48 +0530

    manipulate

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 $(document).ready(() => {
-	let myData = "http://www.omdbapi.com/?i=tt3896198&apikey=bfabb086";
-  //alert(myData);
-  console.log(myData);
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    async: true,
-    url: 'http://www.omdbapi.com/?i=tt3896198&apikey=bfabb086',
-    success: (response) => {
-      $(".result").append(response.Title);
-    //  alert(response);
-    }
-  });
+	$(".sub").click(function() {
+		let def = prompt("hello you","");
+		let abc = $(".searchById").value;
+		alert(abc);
+		alert(def);
+	});
+
+	  $.ajax({
+	    type: 'GET',
+	    dataType: 'json',
+	    async: true,
+	    url: 'http://www.omdbapi.com/?i=tt3896198&apikey=bfabb086',
+	    success: (response) => {
+	      $(".result").append(response.Title);
+	    //  alert(response);
+	    }
+	  });
 });
